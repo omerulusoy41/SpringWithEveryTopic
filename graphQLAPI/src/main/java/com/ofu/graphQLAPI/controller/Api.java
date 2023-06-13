afe5288b441f4365dea8a831758b837a22448681
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-06-13 13:12:04 +0300

    post request is ok.

--- graphQLAPI/src/main/java/com/ofu/graphQLAPI/controller/Api.java
+++ graphQLAPI/src/main/java/com/ofu/graphQLAPI/controller/Api.java
@@ -4,6 +4,7 @@ package com.ofu.graphQLAPI.controller;
 import com.ofu.graphQLAPI.service.GraphQLRepository;
 import com.ofu.graphQLAPI.model.Author;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -27,4 +28,16 @@ public class Api {
     public Optional<Author> getPersonByID(@Argument Long id){
         return repo.findById(id);
     }
+
+    @MutationMapping
+    public Author addPerson(@Argument Input authorIn){
+        Author aut = new Author();
+        aut.setId(authorIn.id);
+        aut.setName(authorIn.name);
+        repo.save(aut);
+        return aut;
+    }
+
+    record Input(int id,String name){}
+
 }
--- graphQLAPI/src/main/java/com/ofu/graphQLAPI/model/Author.java
+++ graphQLAPI/src/main/java/com/ofu/graphQLAPI/model/Author.java
@@ -3,13 +3,18 @@ package com.ofu.graphQLAPI.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.awt.print.Book;
-import java.util.List;
-
 @Document("person")
 public class Author {
 
     @Id
     public long id;
     public String name;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
