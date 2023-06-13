1005f4222d1493ebcd30278c8aade48ee86e8c7a
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-06-13 11:09:53 +0300

    dependency of project is  ready

---
+++ graphQLAPI/src/main/java/com/ofu/graphQLAPI/GraphQlapiApplication.java
@@ -0,0 +1,13 @@
+package com.ofu.graphQLAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlapiApplication.class, args);
+	}
+
+}
---
+++ graphQLAPI/src/test/java/com/ofu/graphQLAPI/GraphQlapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofu.graphQLAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
