b7d75e9f0c4e72ab38e8047b54dd29bfe54d3ffc
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-06-16 08:43:10 +0300

    h2 database is ok.

---
+++ SpringCloud/SpringwithMikroservices/src/main/java/com/ofu/SpringwithMikroservices/SpringwithMikroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ofu.SpringwithMikroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithMikroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithMikroservicesApplication.class, args);
+	}
+
+}
---
+++ SpringCloud/SpringwithMikroservices/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE person (
+    ID int,
+    Name varchar(40)
+);
+
+INSERT INTO PERSON (ID, NAME) VALUES (1, 'Omer');
+INSERT INTO PERSON (ID, NAME) VALUES (2, 'Ali');
+INSERT INTO PERSON (ID, NAME) VALUES (3, 'Ahmet');
