abd78e3fae33b84b75dcab5ef8a1404196b697a0
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-06-16 15:24:30 +0300

    mysql container start and properties is completed.

--- SpringCloud/SpringwithMikroservices/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
---CREATE TABLE person (
---    ID int,
---    Name varchar(40)
---);
-
-INSERT INTO PERSON (ID, NAME) VALUES (1, 'Omer');
-INSERT INTO PERSON (ID, NAME) VALUES (2, 'Ali');
-INSERT INTO PERSON (ID, NAME) VALUES (3, 'Ahmet');
