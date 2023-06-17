a3579e863dfec1419f406d81105f5ac02e8033bb
Ömer Faruk <ulusoyomerfaruk29@gmail.com>
2023-06-17 15:41:02 +0300

    h2-service is ok.

---
+++ SpringCloud/Test-for-person-service-comminication/src/main/java/com/ofu/Testforpersonservicecomminication/TestForPersonServiceComminicationApplication.java
@@ -0,0 +1,13 @@
+package com.ofu.Testforpersonservicecomminication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForPersonServiceComminicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForPersonServiceComminicationApplication.class, args);
+	}
+
+}
